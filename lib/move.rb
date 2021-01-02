363110cbde9d34449fcc41388ae284ee8164bc64
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-02 00:27:12 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, character = "X")
-  board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   board[0] = character
 end
 
